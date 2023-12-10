47792abd756792a54ffce564daa9dabd6c8487e5
Harmanpreet Singh <harmanp397@gmail.com>
2023-12-10 17:10:57 +0530

    10th dec weekly analysis

--- src/pages/Page/Weekly.tsx
+++ src/pages/Page/Weekly.tsx
@@ -1,6 +1,8 @@
 import FooterNavBtn from "@/components/FooterNavBtn";
 import TitleName from "@/components/TitleName";
 import React from "react";
+import img1 from "../../../public/charts/weekly/gj-10.jpg";
+import img2 from "../../../public/charts/weekly/uc-10.jpg";
 
 export default function Weekly() {
   return (
@@ -8,14 +10,14 @@ export default function Weekly() {
       <TitleName />
       <h1 className="heading">Weekly Market Insight</h1>
       <p className="italic">&quot;Consistency is the key to Success&quot;</p>
-      <p >
-        Welcome to &quot;Weekly Market Insights,&quot; Being consistent is better than
-        relying solely on luck.
+      <p>
+        Welcome to &quot;Weekly Market Insights,&quot; Being consistent is
+        better than relying solely on luck.
       </p>
       <p>
-        That&apos;s precisely why I&apos;ve decided to provide a weekly update on GBPJPY
-        and USDCAD. Each week, I&apos;ll be sharing insights into trends,
-        predictions, and relevant news to keep you well-informed and
+        That&apos;s precisely why I&apos;ve decided to provide a weekly update
+        on GBPJPY and USDCAD. Each week, I&apos;ll be sharing insights into
+        trends, predictions, and relevant news to keep you well-informed and
         strategically positioned in these currency pairs. Stay tuned for your
         regular dose of market updates!
       </p>
@@ -29,6 +31,38 @@ export default function Weekly() {
         >
           forexfactory.com
         </a>
+      </p>
+      <h2>Weekly Analysis-10 December</h2>
+      <h2>GJ Analysis</h2>
+
+      <img
+        src={img1.src}
+        alt="GJ 10th Dec chart analysis"
+        className="img-width-para"
+      />
+      <p className="img-info-para">Fig1.1- GJ 10th Dec chart analysis</p>
+      <p>
+        This week, the market experienced a significant meltdown, barely
+        touching 178.500 before rebounding. It seems like GBPJPY will be
+        entering a sideways market phase, a scenario that has occurred quite
+        frequently. While I&apos;m not entirely certain, I have a hunch that it might
+        remain in this sideways pattern for at least a week. Keep an eye on the
+        charts for confirmation!
+      </p>
+
+      <h2>USDCAD Analysis</h2>
+      <img
+        src={img2.src}
+        alt="USDCAD 10th Dec chart analysis"
+        className="img-width-para"
+      />
+      <p className="img-info-para">Fig1.2- USDCAD 10th Dec chart analysis</p>
+      <p>
+        USDCAD is currently in a consolidation phase, resembling a box pattern.
+        It has the potential to break out in either direction, but it&apso;s worth
+        noting that often, the initial breakout tends to be a fakeout. Exercise
+        caution and stay vigilant in your trading decisions considering this
+        pattern.
       </p>
       <FooterNavBtn />
     </main>
